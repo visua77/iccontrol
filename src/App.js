93909c91b250f7c8037a473aada2747d403457c1
Monica Binnquist <monicabinnquist@monicas-air.mynet>
2020-09-08 00:33:56 +0200

    fixed the img-array with props

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import data from './json/qib2.json'
 console.log(data)
 
 export const App = () => {
+  
   return (
     <div className="mainWrapper">
       <div className="header"><img src="./img/ICCMS_low-q-logo_2_1.png" alt="logoimg"/><span className="toggle">Toggle</span></div>
       <div className="mainContainer">
-      {data.map((item) => (
-        <Buttons name={item.ButtonText} key={item.ButtonText} />
+      {data.map((item, index) => (
+        
+        <Buttons name={item.ButtonText} img={index} key={item.ButtonText} />
       ))}
       </div>
       <div className="footer"><span className="copyright">Copyright Niclas Binnquist 2020</span></div>
--- src/Buttons.js
+++ src/Buttons.js
@@ -3,14 +3,13 @@ import Images from './Images'
 
 export const Buttons = (props) => {
 
-    
-
   return (
     <div className="buttonContainer"> 
       <button onClick={(e) => console.log(e.target)}><span className="caption">{props.name}</span>
-      <Images /></button>
+      <img src={`./img/${props.img}.jpg`} alt="test" /></button>
     </div>
   )
 }
+//{`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
 
 export default Buttons
