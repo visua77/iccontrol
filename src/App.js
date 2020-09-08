299c453b5cf734f0d9f11f3fcea5e0dab7b0ae01
Monica Binnquist <monicabinnquist@monicas-air.mynet>
2020-09-08 16:12:04 +0200

    toggle state is now working, very nice

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import Buttons from './components/Buttons'
 import data from './json/qib2.json'
 import data2 from './json/qib3.json'
 
-//console.log(data)
-
 export const App = () => {
 
   const [toggle, setToggle] = useState(false)
+  const [icdata, setIcdata] = useState(data)
 
+  console.log(icdata)
+  
   const toggler = () => {
     setToggle(prev => !prev)
+    if(icdata === data){
+      setIcdata(data2)
+    }
+    if(icdata === data2){
+      setIcdata(data)
+    }
   }
   
   return (
     <div className="mainWrapper">
       <div className="header"><div><img src="./img/ICCMS_low-q-logo_2_1.png" alt="logoimg"/></div><span className={toggle ? 'active' : 'default'} onClick={toggler}>Toggle</span></div>
       <div className="mainContainer">
-      {data.map((item, index) => (
+      {icdata.map((item, index) => (
         <Buttons name={item.ButtonText} img={index} key={item.ButtonText} />
       ))}
       </div>
