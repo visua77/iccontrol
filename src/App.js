253e30e185b42032a983da43e897570e1adc0f10
Monica Binnquist <monicabinnquist@monicas-air.mynet>
2020-09-08 15:39:48 +0200

    adding state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import Buttons from './Buttons'
+import React ,{useState} from 'react'
+import Buttons from './components/Buttons'
 import data from './json/qib2.json'
+import data2 from './json/qib3.json'
 
-console.log(data)
+//console.log(data)
 
 export const App = () => {
+
+  const [toggle, setToggle] = useState(false)
+
+  const toggler = () => {
+    setToggle(prev => !prev)
+  }
   
   return (
     <div className="mainWrapper">
-      <div className="header"><div><img src="./img/ICCMS_low-q-logo_2_1.png" alt="logoimg"/></div><span className="toggle">Toggle</span></div>
+      <div className="header"><div><img src="./img/ICCMS_low-q-logo_2_1.png" alt="logoimg"/></div><span className={toggle ? 'active' : 'default'} onClick={toggler}>Toggle</span></div>
       <div className="mainContainer">
       {data.map((item, index) => (
         <Buttons name={item.ButtonText} img={index} key={item.ButtonText} />
       ))}
       </div>
-      <div className="footer"><span className="copyright">Copyright Niclas Binnquist 2020</span></div>
+      <div className="footer"><span className="copyright">Copyright <a href="mailto:nicznet@hotmail.com">Niclas Binnquist</a> 2020</span></div>
     </div>
   )
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Images from './Images'
 
 export const Buttons = (props) => {
 
   return (
     <div className="buttonContainer"> 
       <button onClick={(e) => console.log(e.target)}><span className="caption">{props.name}</span>
-      <img src={`./img/${props.img}.jpg`} alt="test" className="sportImg"/></button>
+      <img src={`./img/${props.img}.jpg`} alt="image" className="sportImg"/></button>
     </div>
   )
 }
