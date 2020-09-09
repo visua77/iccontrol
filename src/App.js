2ca60e9f694b934821ec49d4496953a14a061e88
Monica Binnquist <monicabinnquist@monicas-air.mynet>
2020-09-09 14:32:22 +0200

    added a onmouseover-text as well

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Buttons from './components/Buttons'
 import data from './json/qib2.json'
 import data2 from './json/qib3.json'
 
-export const App = () => {
+export const App = (props) => {
 
   //Setting state
   const [toggle, setToggle] = useState(false)
   const [icdata, setIcdata] = useState(data)
   const [imgset, setImgset] = useState(1)
+  const [value, setValue] = useState()
 
   const toggler = () => {
     setToggle(prev => !prev)
@@ -21,17 +22,19 @@ export const App = () => {
       setImgset(1)
       }
   }
-  
+
   return (
     <div className="mainWrapper">
       <div className="header"><div><a href="http://www.iccmediasport.com/"target="new"><img src="./img/ICCMS_low-q-logo_2_1.png" alt="logoimg"/></a></div>
-      <i className={toggle ? 'fas fa-toggle-on' : 'fas fa-toggle-off'} onClick={toggler}></i></div>
+      <div className="textinfo">{value}</div><i className={toggle ? 'fas fa-toggle-on' : 'fas fa-toggle-off'} onClick={toggler}></i></div>
       <div className="mainContainer">
       {icdata.map((item, index) => (
-        <Buttons name={item.ButtonText} id={item.QuickButtonGroupOid} img={index} key={item.ButtonText} set={imgset} />
+        <Buttons name={item.ButtonText} id={item.QuickButtonGroupOid} img={index} key={item.ButtonText} set={imgset} setValue={setValue} />
       ))}
       </div>
       <div className="footer"><span className="copyright">Copyright <a href="mailto:nicznet@hotmail.com">Niclas Binnquist</a> 2020</span></div>
     </div>
   )
 }
+
+
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export const Buttons = (props) => {
 
-  //Setting variable for detecting which imgset to use
+//Setting variable for detecting which imgset to use
   let imgno 
  
   if (props.set === 1) {
@@ -12,15 +12,30 @@ export const Buttons = (props) => {
     imgno = 2
   }
 
+  //handleClick func
+  const handleClick = () => {
+    console.log('The button with id'+props.id+' was clicked!')
+    }
+
+  //onMouseOver func
+
+    const handleMouseOver = () => {
+      props.setValue('The button with id'+props.id+' was hovered!')
+    }
+
   return (
     <div className="buttonContainer"> 
-      <button id={props.id}onClick={(e) => console.log('The button with id'+props.id+' was clicked!')}><span className="caption">{props.name}</span>
-      <img src={`./img/${props.img}_${imgno}.jpg`} alt="image" className="sportImg"/></button>
+      <button id={props.id} onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={() => props.setValue('')}><span className="caption">{props.name}</span>
+  <img src={`./img/${props.img}_${imgno}.jpg`} alt="image" className="sportImg"/></button>
     </div>
   )
 }
 
 export default Buttons
+
+
+
+
 
 
 
